0147b9cd0b4102aabc9b9bee34a9710daf191372
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2023-06-24 12:32:37 +0530

    updated

--- server/db.js
+++ server/db.js
@@ -1,10 +1,7 @@
 import mysql from 'mysql';
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'myblog'
-});
+const urlDB = `mysql://root:bhVatF8dN4WJ4I2IBIpi@containers-us-west-62.railway.app:5887/railway`;
+
+const db = mysql.createConnection(urlDB);
 
 export default db;
