e8662b8c1603647612d77ac6e9c939fef5068183
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2023-06-24 22:29:33 +0530

    updated

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2';
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLPASSWORD}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 const db = mysql.createConnection(urlDB);
 
